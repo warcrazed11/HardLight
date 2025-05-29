4fd9a0fcbef03ca11c67cf954532a38604fe26df
Cygnall <31330700+Cygnall@users.noreply.github.com>
2025-05-29 09:56:53 -0400

    Edited Gamemode Stuff
    Fixed the CVar that was pointing at the default map to point at ours, edited the server description some, reverted map prototypes back to their original version, added our modified version of Core as "HardCore"

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -69,7 +69,7 @@ public sealed partial class CCVars
     ///     Controls the game map prototype to load. SS14 stores these prototypes in Prototypes/Maps.
     /// </summary>
     public static readonly CVarDef<string>
-        GameMap = CVarDef.Create("game.map", "Frontier", CVar.SERVERONLY); // Frontier: string.Empty<Frontier
+        GameMap = CVarDef.Create("game.map", "HardCore", CVar.SERVERONLY); // Frontier: string.Empty<Frontier
 
     /// <summary>
     ///     Controls whether to use world persistence or not.
