28f280a8b5299e1931d17caa7503ee1c117c44e3
Sirenfal <avosirenfal@gmail.com>
2025-06-05 05:38:31 -0400

    * add support for players connecting/disconnecting during a shift * normalize payout prototypes to unit value * add cvar for payout delay

---
+++ Content.Server/_Hardlight/CVars.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.Configuration;
+
+namespace Content.Server._Hardlight;
+
+[CVarDefs]
+public sealed class HardlightCVars
+{
+    /// <summary>
+    /// How often station staff wages are paid.
+    /// </summary>
+    public static readonly CVarDef<int> StationPayDelay =
+        CVarDef.Create(
+            "station_pay.delay",
+            3600,
+            CVar.SERVERONLY,
+            "how often station staff wages are paid"
+        );
+}
