5a9fa3a76dee970f5c65eb8bfb1b7220ca3de526
fenndragon <fenndragon@gmail.com>
2025-07-15 23:07:22 -0600

    Update UnclonableComponent.cs

--- Content.Server/_NF/Traits/Assorted/UnclonableComponent.cs
+++ Content.Server/_NF/Traits/Assorted/UnclonableComponent.cs
@@ -1,22 +1,22 @@
-using Robust.Shared.GameStates;
+//using Robust.Shared.GameStates;
 
-namespace Content.Server._NF.Traits.Assorted;
+//namespace Content.Server._NF.Traits.Assorted;
 
 /// <summary>
 /// This is used for the uncloneable trait.
 /// </summary>
-[RegisterComponent]
-public sealed partial class UncloneableComponent : Component
-{
+//[RegisterComponent]
+//public sealed partial class UncloneableComponent : Component
+//{
     /// <summary>
     /// A field to define if we should display the "Genetic incompatibility" warning on health analysers
     /// </summary>
-    [DataField, AutoNetworkedField]
-    public bool Analyzable = true;
+//    [DataField, AutoNetworkedField]
+//    public bool Analyzable = true;
 
     /// <summary>
     /// The loc string used to provide a reason for being unrevivable
     /// </summary>
-    [DataField, AutoNetworkedField]
-    public LocId ReasonMessage = "cloning-console-uncloneable-trait-error";
-}
+//    [DataField, AutoNetworkedField]
+//    public LocId ReasonMessage = "cloning-console-uncloneable-trait-error";
+//}
