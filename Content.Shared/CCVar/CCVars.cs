8a6dd6e99e96cf30b90ff880aa75ab298b2ffa5b
fenndragon <fenndragon@gmail.com>
2025-07-30 16:15:35 -0600

    Update CCVars.cs

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -54,6 +54,19 @@ public sealed partial class CCVars : CVars
 
     public static readonly CVarDef<bool> CrawlUnderTables =
         CVarDef.Create("rest.crawlundertables", true, CVar.SERVER | CVar.ARCHIVE);
+        
+    public static readonly CVarDef<bool> AutoVoteEnabled =
+            CVarDef.Create("vote.autovote_enabled", true, CVar.SERVERONLY); // Floof
+
+        /// Automatically starts a map vote when returning to the lobby.
+        /// Requires auto voting to be enabled.
+    public static readonly CVarDef<bool> MapAutoVoteEnabled =
+            CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
+
+        /// Automatically starts a gamemode vote when returning to the lobby.
+        /// Requires auto voting to be enabled.
+    public static readonly CVarDef<bool> PresetAutoVoteEnabled =
+            CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
 
     #endregion
 }
