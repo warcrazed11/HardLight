0bee1871e79b5b133705d212ceeb455de79e843c
warcrazed11 <rjackcrozier@gmail.com>
2026-03-10 20:08:12 -0400

    Adds security officer comp, Makes muted blacklist sec and command

---
+++ Content.Server/Security/SecurityOfficerComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Security.Components;
+
+/// <summary>
+/// Given to Security at round start. Used for assigning traitors to kill Sec.
+/// </summary>
+[RegisterComponent]
+public sealed partial class SecurityOfficerComponent : Component
+{
+
+}
